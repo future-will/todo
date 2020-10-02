029f8259351eb06a64dbd16b73138c115475d979
Will Kuhlke <willkuhlke@Wills-MacBook-Pro-2.local>
2020-10-02 17:46:06 -0400

    still dont get git

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ function addNewList() {
 
 function addListItem() {
     console.log('hello world');
+    const list = document.getElementById("grocery-list");
+    const itemInput = document.getElementById("new-list-item");
+    const newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+function deleteListItem(item){
+	//remove li element from OL element (item.parentNode)
+	item.parentNode.removeChild(item);
+}
+function completeListItem(item){
+	if (item.checked){
+		//true if input is checked
+		item.parentNode.style.textDecoration = "line-through";
+	}
+	else{
+		item.parentNode.style.textDecoration = "none";
+	}	
 }
